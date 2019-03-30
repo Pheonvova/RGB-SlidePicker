13121a530d48779e2c6e35c1cac1378e25c0b34e
Pheon <pheon@imac-pheon.local>
2019-03-30 10:56:38 +0200

    Initial Commit

---
+++ RGB SlidePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGB SlidePicker
+//
+//  Created by Pheon on 30.03.2019.
+//  Copyright © 2019 PheDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
