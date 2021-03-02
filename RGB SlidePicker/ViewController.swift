d8d769f35f9bb4c4cfa2cb249f2c3c513876424e
Pheonvova <pheon1310@gmail.com>
2021-03-02 12:02:26 +0200

    test commit

--- RGB SlidePicker/ViewController.swift
+++ RGB SlidePicker/ViewController.swift
@@ -7,14 +7,69 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorDemonstration: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var textValueRed: UITextField!
+    @IBOutlet var textValueGreen: UITextField!
+    @IBOutlet var textValueBlue: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        redLabel.textColor = .red
+        redLabel.text = "Red:"
+        redLabel.font = redLabel.font.withSize(12)
+        textValueRed.font = textValueRed.font?.withSize(12)
+        
+        greenLabel.textColor = .green
+        greenLabel.text = "Green:"
+        greenLabel.font = greenLabel.font.withSize(12)
+        textValueGreen.font = textValueGreen.font?.withSize(12)
+        
+        blueLabel.textColor = .blue
+        blueLabel.text = "Blue:"
+        blueLabel.font = blueLabel.font.withSize(12)
+        textValueBlue.font = textValueBlue.font?.withSize(12)
+        
     }
 
 
+    @IBAction func redSliderChange(_ sender: UISlider) {
+        
+        redValueLabel.text = String(redSlider.value)
+        textValueRed.text = String(redSlider.value)
+
+    }
+    
+    @IBAction func greenSliderChange(_ sender: UISlider) {
+        
+        greenValueLabel.text = String(greenSlider.value)
+        textValueGreen.text = String(greenSlider.value)
+        
+        
+    }
+    
+    @IBAction func blueSliderChange(_ sender: UISlider) {
+        
+        blueValueLabel.text = String(blueSlider.value)
+        textValueBlue.text = String(blueSlider.value)
+        
+    }
 }
 
